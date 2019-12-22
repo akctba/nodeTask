c9f2e3547892f1210146d47036fb022ebf07d986
Alex Kayser <akctba@gmail.com>
2019-12-21 19:20:07 -0800

    consign modifications

--- config/server.js
+++ config/server.js
@@ -1,6 +1,11 @@
 var express = require('express');
+var consign = require('consign');
+
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+// map all files in the routes folder as routes in the app.
+consign().include('app/routes').into(app);
 
 module.exports = app;
