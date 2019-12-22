583867820e0fbfe1d60ceb9a4ca1830a5202624c
Alex Kayser <akctba@gmail.com>
2019-12-22 14:45:48 -0800

    form noticia

--- config/server.js
+++ config/server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var consign = require('consign');
+var bodyparser = require('body-parser');
 
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+//body-parser (midleware)
+app.use(bodyparser.urlencoded({extended: true}));
 
 // map all files in the routes folder as routes in the app.
 consign()
