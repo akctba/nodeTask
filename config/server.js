cd162c8192a199361472506fe38e86cbd699861a
Alex Kayser <akctba@gmail.com>
2019-12-23 09:52:10 -0800

    unistall validator to install the previous version

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var consign = require('consign');
 var bodyparser = require('body-parser');
+var expressValidator = require('express-validator');
 
 var app = express();
 app.set('view engine', 'ejs');
@@ -8,6 +9,7 @@ app.set('views', './app/views');
 
 //body-parser (middleware)
 app.use(bodyparser.urlencoded({extended: true}));
+app.use(expressValidator());
 
 // map all files in the routes folder as routes in the app.
 consign()
