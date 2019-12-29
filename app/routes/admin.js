41db92fded198dc625440b12b4b8e2625bee2894
Alex Kayser <akctba@gmail.com>
2019-12-29 13:03:09 -0800

    change variable name

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -4,7 +4,7 @@ module.exports = function(application) {
     });
 
     application.post('/noticias/salvar', function (req, res) {
-        var currNoticia = req.body;
+        var newNoticia = req.body;
         //res.send(noticias);
 
         //console.log(noticia);
@@ -18,7 +18,7 @@ module.exports = function(application) {
 
         var error = req.validationErrors();
         if(error) {
-            res.render("admin/form_add_noticia", {invalid: error, noticia: currNoticia});
+            res.render("admin/form_add_noticia", {invalid: error, noticia: newNoticia});
             return;
         }
 
@@ -26,7 +26,7 @@ module.exports = function(application) {
         var connection = application.config.dbConnection();
         var noticiasDAO = new application.app.models.NoticiasDAO(connection);
 
-        noticiasDAO.saveNoticias(currNoticia, function (error, result) {
+        noticiasDAO.saveNoticias(newNoticia, function (error, result) {
             //res.render("noticias/noticias", {noticias: result});
             res.redirect('/news');
         });
