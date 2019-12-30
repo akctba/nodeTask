cd9dd6368bbb332805984b588240b68c52392a57
Alex Kayser <akctba@gmail.com>
2019-12-30 09:39:15 -0800

    merging similar routes

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -11,4 +11,14 @@ module.exports = function (application) {
 
         
     });
+
+    application.get('/noticia', function (req, res) {
+
+        var connection = application.config.dbConnection();
+        var noticiasDAO = new application.app.models.NoticiasDAO(connection);
+
+        noticiasDAO.getNoticia(function (error, result) {
+            res.render("noticias/noticia", {noticias: result});
+        });
+    });
 }
