6e51392919dca18a54c1da80d21306318893109d
Alex Kayser <akctba@gmail.com>
2019-12-30 10:11:13 -0800

    home controller

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,6 +1,7 @@
-module.exports = function(app) {
-    app.get('/', function (req, res) {
+module.exports = function (application) {
+    application.get('/', function (req, res) {
         //res.send("<html><body>Central de noticias</body></html>");
-        res.render("home/index");
+        //res.render("home/index");
+        application.app.controllers.home.index(application, req, res);
     });
 }
