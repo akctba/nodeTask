3feb4c4def6042137da919bbd51a9fe1239d9970
Alex Kayser <akctba@gmail.com>
2019-12-30 18:29:05 -0800

    get noticia

--- app/controllers/noticias.js
+++ app/controllers/noticias.js
@@ -13,7 +13,10 @@ module.exports.noticia = function (application, req, res) {
     var connection = application.config.dbConnection();
     var noticiasDAO = new application.app.models.NoticiasDAO(connection);
 
-    noticiasDAO.getNoticia(function (error, result) {
+    var id_noticia = req.query;
+    //console.log(req);
+
+    noticiasDAO.getNoticia(id_noticia, function (error, result) {
         res.render("noticias/noticia", {
             noticia: result
         });
